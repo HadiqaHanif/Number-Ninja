acf46721bf66b65c0ebfc9aa8660880becaeb3be
Hadiqa Hanif <hadiqaa201@gmail.com>
2026-08-17 14:18:25 +0500

    Add files via upload

---
+++ take_input.py
@@ -0,0 +1,8 @@
+
+def user_input():
+    while True:
+        try:
+            number = float(input("Enter a number needed for calculation: "))
+            return number
+        except ValueError:
+            print("Invalid Input. Try Again!")
